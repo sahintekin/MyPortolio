7dadf82c8f22b2c0723170895d43d1ba2be8e6c4
sahintekin <ahmetsahintekin1@gmail.com>
2024-12-04 00:02:56 +0300

    Add project files.

---
+++ MyPortolio/DAL/Context/MyPortfolioContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPortolio.DAL.Entities;
+
+namespace MyPortolio.DAL.Context
+{
+    public class MyPortfolioContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=AHMET\\SQLEXPRESS;initial Catalog=MyPortfolioDb; integrated Security=true;");
+        }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<Testiomonial> Testiomonials { get; set; }
+    }
+}
---
+++ MyPortolio/DAL/Entities/Testiomonial.cs
@@ -0,0 +1,14 @@
+﻿namespace MyPortolio.DAL.Entities
+{
+    public class Testiomonial
+    {
+        public int TestiomonialId { get; set; }
+        public string NameSurname { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+
+        
+
+    }
+}
